946217c79e037413948869157ccdf724f3042edd
NathanBaraka <nathanbaraka23@gmail.com>
2024-11-22 12:07:56 +0000

    react_immutable

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,26 +1,15 @@
-// 5-merge.js
-import { List, Map } from 'immutable';
+import { Map, List } from 'immutable';
 
-/**
- * Concatenates two arrays into an Immutable List.
- *
- * @param {Array} page1 - The first array.
- * @param {Array} page2 - The second array.
- * @returns {List} - An Immutable List containing the values of the two pages.
- */
 export function concatElements(page1, page2) {
-  return List([...page1, ...page2]);
+  const list1 = List(page1);
+  const list2 = List(page2);
+
+  return list1.concat(list2);
 }
 
-/**
- * Merges two objects into an Immutable List.
- *
- * @param {Object} page1 - The first object.
- * @param {Object} page2 - The second object.
- * @returns {List} - An Immutable List containing the values of the two pages.
- */
 export function mergeElements(page1, page2) {
-  const mergedMap = Map(page1).merge(Map(page2));
-  return mergedMap.valueSeq().toList(); // Get the values as a List
-}
+  const map1 = Map(page1);
+  const map2 = Map(page2);
 
+  return map1.merge(map2);
+}
